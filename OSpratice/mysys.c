ed32459ced92e3344dc3b769f1e30f064101e6c6
ZYY <18834827690@163.com>
2018-06-17 01:16:09 +0800

    调用system "实现重定向"
    This reverts commit 9b8e3f83072d37b450fa2d4d7392ef12960a2e63.

--- OSpratice/mysys.c
+++ OSpratice/mysys.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include </home/zyy/OSpratice/mysys.h>
+#include </home/zyy/OSpratice/FileRW/mysys.h>
 int main()
 {
     printf("--------------------------------------------------\n");
     mysystem("echo HELLO WORLD");
-    printf("--------------------------------------------------\n");
     printf("--------------------------------------------------\n");
     mysystem("ls /");
     printf("--------------------------------------------------\n");
